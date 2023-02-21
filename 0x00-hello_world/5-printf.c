029d74ad71f6352b50ef859e27de10dff7e38b48
nessoro96 <emanueldaudi1002gmail.com>
2023-02-20 20:45:10 -0800

    5-printf.c changes on space

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf ("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
