dfe5e63e09f041beb0662e8701fa8c77af8a9d41
nessoro96 <emanueldaudi1002gmail.com>
2023-02-20 21:14:22 -0800

    6-size.c changes on printf'

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int b;
 long long int c;
 char d;
 float f;
-print("size of int:%lu bytes(s)\n",(unsigned long) sizeof(a));
-print("size of long int:%lu bytes(s)\n",(unsigned long) sizeof(b));
-print("size of long long int:%lu bytes(s)\n",(unsigned long) sizeof(c));
-print("size of char:%lu bytes(s)\n",(unsigned long) sizeof(d));
-print("size of float:%lu bytes(s)\n",(unsigned long) sizeof(f));
+printf("size of int:%lu bytes(s)\n",(unsigned long) sizeof(a));
+printf("size of long int:%lu bytes(s)\n",(unsigned long) sizeof(b));
+printf("size of long long int:%lu bytes(s)\n",(unsigned long) sizeof(c));
+printf("size of char:%lu bytes(s)\n",(unsigned long) sizeof(d));
+printf("size of float:%lu bytes(s)\n",(unsigned long) sizeof(f));
 return (0);
 }
