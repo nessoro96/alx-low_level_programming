de5fb32e17e06c44990a2fe39a2fd62a31303cb0
nessoro96 <emanueldaudi1002gmail.com>
2023-02-27 11:05:47 -0800

    0x01. C - Variables, if, else, while

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- *main - Entry point
- *
- * return: Always 0 (success)
- */
+*main - A program that print a line using the printf function
+*
+* return: Always 0 (success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
---
+++ README.md
@@ -0,0 +1,2 @@
+0x00-hello_world
+0x01. C - Variables, if, else, while
