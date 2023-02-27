5c8b4ca7ad6019bf4ae2e3fa4e9edb6403e50b20
nessoro96 <emanueldaudi1002gmail.com>
2023-02-27 14:03:39 -0800

    last changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-*main - A program that print a line using the printf function
-*
-* return: Always 0 (success)
-*/
+ * main - Hello, printf
+ * Description: Using printf to print text
+ * Return: 0
+ */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
