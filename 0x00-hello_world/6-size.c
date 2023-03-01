4761849ab7e9fee91e3b5c2a5e9e8c40cb580727
nessoro96 <emanueldaudi1002gmail.com>
2023-03-01 01:27:28 -0800

    tassk no 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Size is not grandeur, and territory does not make a nation
+ * Description - Print the size of various types
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-printf("size of a char: %lu bytes(s)\n", sizeof(d));
-printf("size of an int: %lu bytes(s)\n", sizeof(a));
-printf("size of a long int: %lu bytes(s)\n", sizeof(b));
-printf("size of a long long int: %lu bytes(s)\n", sizeof(c));
-printf("size of a float: %lu bytes(s)\n", sizeof(f));
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
