69cbd97692fa24bac23d1899747d342106db3687
nessoro96 <emanueldaudi1002gmail.com>
2023-03-04 12:02:06 -0800

    1-last-digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,28 +2,35 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - deteremine the last digit of the number stored in the variable.
-*
-* Return: Always 0 (success)
-*
+ * main -Entry point
+ * Description: a program that store differnt value everytime
+ * that show the string 'lasit digit of'
+ * number,if the last digit of n is greater than 5 print is greater than 5
+ * if the last digit of n is 0: the string and is 0
+ * if the last digit is less than 6 and not 0:print is less than 6 and not 0
+ * return: Always (success)
 */
 
-int main(void)
+int main (void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if ((n % 10) > 5)
-{
-printf("last digit of %d is %d and is greater than 5 \n", n, n % 10);
-}
-else if ((n % 10) < 6 && (n % 10) != 0)
-{
-printf("last digit of %d is %d and is less than 6 and not 0 \n", n, n % 10);
-}
-else
-{
-printf("last digit of %d is %d and is 0\n", n, n % 10);
-}
-return (0);
+	int n;
+int lastdgt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdgt = n % 10;
+	printf("Last digit of %d is ", n);
+	if (lastdgt > 5)
+	{
+		printf("Last digit of %d is %d and is  greater than 5\n",n,  lastdgt);
+	}
+	else if (lastdgt == 0)
+	{
+		printf("Last digit of %d is %d  and is 0\n", n,  lastdgt);
+	}
+	else if(lastdgt < 6 && lastdgt != 0)
+	{
+		printf("Last digit of %d  is  %d less than 6 and not 0\n", n,  lastdgt);
+	}
+	return (0);
 }
