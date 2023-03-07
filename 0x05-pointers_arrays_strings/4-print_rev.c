6a3eee2f8a70167f7f72f03e6100948d1f57023d
nessoro96 <emanueldaudi1002gmail.com>
2023-03-07 03:33:14 -0800

    task no 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * Description: Prints string in reverse
+ * @s: Integer
+ * Return: char
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+
+while (s[i])
+i++;
+while (i--)
+_putchar(s[i]);
+_putchar('\n');
+}
